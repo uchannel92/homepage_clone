e8c25569cf10b9673b01d34792960e7a631332a4
uchannel92 <uchenna_attah@hotmail.com>
2021-07-03 10:58:51 +0100

    Issu with h1 font being too large. Have removed h1 styling from app.js and moved to css using media queries

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const colors = ['#4285F4','#DB4437','#FECA1D','#4285F4','#0F9D58','#DB4437']
 
 let h1 = document.querySelector('#h1')
 let span = document.createElement('span')
-h1.style.fontSize = '7em'
+// h1.style.fontSize = '7em'
 
 let title = 'Google'
 let splitString = title.split('')
